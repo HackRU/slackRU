30c87462374eeb642a55f916841395c06d9d6508
Sail338 <srihari.shankar1@gmail.com>
2017-09-20 19:46:08 -0400

    started to write the route

--- serverside.py
+++ serverside.py
@@ -1,4 +1,11 @@
-from Flask import flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/pairmentor")
+def pairMentor(methods = ['POST']):
+    return "Hello World!"
 
 #the twilio end point that will text mentors
+def pairMentorWithHacker():
 
