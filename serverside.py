9d6649543a62986d877e109057496de31adc2d54
Sail338 <srihari.shankar1@gmail.com>
2017-09-25 19:52:45 -0400

    build keywords list

--- serverside.py
+++ serverside.py
@@ -1,11 +1,36 @@
 from flask import Flask
-
+import sqlite3
+import config
 app = Flask(__name__)
+dbpath = config.dbpath
+def get_db():
+    """
+    From the Flask Websitei, probs best practice to connect to the databse this way
+    """
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+def query_db(query, args=(), one=False):
+    """
+    Query function from flask documentation to avoid the usage of a raw cursor
+    """
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 @app.route("/pairmentor")
 def pairMentor(methods = ['POST']):
     return "Hello World!"
 
 #the twilio end point that will text mentors
-def pairMentorWithHacker():
+def pairMentorWithHacker(comment:str,username:str):
+    query = query_db('SELECT * from Keywords')
+    #build a list of keywords
+    keywordlist = []
+    for keyword in query:
+        keywordlist.append(keyword['keyword'])
 
+
+    
