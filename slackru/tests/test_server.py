24dc148ffed83651f551359d27c8c8205bbc46e4
Bryan Bugyi <bryanbugyi34@gmail.com>
2017-12-29 18:49:21 -0500

    Add Tests

--- slackru/tests/test_server.py
+++ slackru/tests/test_server.py
@@ -2,21 +2,30 @@ import os
 import unittest
 import flask
 import json
+from datetime import datetime, timedelta
 
 
 class TestServer(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        from slackru.config import getConfig
+        os.environ['SLACK_CONFIG'] = 'testing'
+        flask.testing = True
+
+        from slackru.config import config
         from slackru import create_app
 
-        flask.testing = True
-        config = getConfig['testing']
         if os.path.isfile(config.dbpath):
             os.remove(config.dbpath)
         app = create_app(config)
+        db = app.db.get_db()
 
-        cls.db = app.db.open()
+        mentor_id = db.insertMentor("Bryan Bugyi", "bryan.bugyi", "U86U3G52Q", "Python")
+
+        start = datetime.now().strftime('%Y-%m-%d %H:%M')
+        end = (datetime.now() + timedelta(hours=1)).strftime('%Y-%m-%d %H:%M')
+        db.insertShift(mentor_id, start, end)
+
+        cls.db = db
         cls.app = app
         cls.config = config
 
@@ -25,28 +34,29 @@ class TestServer(unittest.TestCase):
         cls.db.close()
 
     def test_query_db(self):
-        self.db.conn.execute('INSERT INTO mentors \
-                values ("Bryan Bugyi", "Python", "0")')
-        self.db.conn.commit()
         query = self.db.query_db("SELECT * FROM mentors", one=True)
         self.assertEqual(query['name'], "Bryan Bugyi")
         self.assertEqual(query['keywords'], "Python")
-        self.assertEqual(query['id'], 0)
+        self.assertEqual(query['id'], 1)
 
     def test_pairMentor(self):
-        postData = {'data': 'I need help with Python',
-                    'user': 'bbugyi200',
-                    'userid': '12345'}
+        postData = {'question': 'I need help with Python',
+                    'username': 'bryan.bugyi',
+                    'userid': 'U86U3G52Q'}
 
         with self.app.test_client() as client:
             resp = client.post(self.config.serverurl + 'pairmentor', data=postData)
-            self.assertEqual(json.loads(resp.data), postData)
+            self.assertEqual(json.loads(resp.data), ['U86U3G52Q'])
 
     def test_askQuestion(self):
         from slackru.serverside import askQuestion
-        self.assertEqual(0, askQuestion(self.db, "What's 2+2?", "bbugyi200", "12345"))
-        self.assertEqual(1, askQuestion(self.db, "What's 2-2?", "bbugyi200", "12345"))
-        self.assertEqual(2, askQuestion(self.db, "What's 2*2?", "bbugyi200", "12345"))
+        askQuestion(self.db, "What is 2+2?", "bryan.bugyi", "U86U3G52Q")
+
+        actual = [val for val in self.db.query_db("SELECT * FROM questions WHERE question='What is 2+2?'", one=True).values()]
+        expected = [1, "What is 2+2?", 0, "bryan.bugyi", "U86U3G52Q",
+                    datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '["U86U3G52Q"]', None]
+
+        self.assertEqual(expected, actual)
 
     def test_answerQuestion(self):
         pass
