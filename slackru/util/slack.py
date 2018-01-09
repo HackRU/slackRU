e150cdda95d28192571a9480daa0f51d3aafd1aa
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-01-09 15:28:58 -0500

    Define SlackError Exception

--- slackru/util/slack.py
+++ slackru/util/slack.py
@@ -8,27 +8,33 @@ from slackclient import SlackClient
 slack_client = SlackClient(os.environ['SLACK_API_KEY'])
 
 
+class SlackError(Exception): pass
+
+
 def id_to_username(userid):
-    api = slack_client.api_call('users.list')
-    if (api.get('ok')):
-        users = api.get('members')
-        for user in users:
-            if 'name' in user and user.get('id') == userid:
-                return user['name']
+    resp = slack_client.api_call('users.list')
+    users = respErrorCheck(resp)['members']
+    for user in users:
+        if 'name' in user and user['id'] == userid:
+            return user['name']
 
 
 def getDirectMessageChannel(users):
-    try:
-        resp = slack_client.api_call("conversations.open", users=users)
-        return resp['channel']['id']
-    except KeyError as e:
-        raise KeyError(locals())
+    resp = slack_client.api_call("conversations.open", users=users)
+    return respErrorCheck(resp)['channel']['id']
 
 
 def sendMessage(channel, text, attachments=None):
     resp = slack_client.api_call("chat.postMessage", channel=channel, text=text, attachments=attachments, as_user=True)
-    return resp
+    return respErrorCheck(resp)
 
 
 def deleteDirectMessages(channel, ts):
     slack_client.api_call("chat.delete", channel=channel, ts=ts, as_user=True)
+
+
+def respErrorCheck(resp):
+    if resp['ok']:
+        return resp
+    else:
+        raise SlackError(resp['error'])
