a93089656d8f96bd02788b50d050e331d239003b
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-02-09 00:24:43 -0500

    Run SlackBot in new Thread

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
+from threading import Thread
+
 from slackru import create_app
+from slackru.bot.slackbot import SlackBot
+
+bot = SlackBot()
+t = Thread(target=bot.run)
+t.start()
 
 application = create_app()
 
