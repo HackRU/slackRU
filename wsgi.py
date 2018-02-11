f044a79e9eb17a6e8fae2f23c3b20712de7545cf
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-02-11 06:16:42 -0500

    Removed Forked SlackBot Process
    This solution has proved unreliable. We will need to run the SlackBot on
    a different computer.

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,3 @@
-import os
-
 from slackru import create_app
-from slackru.bot.slackbot import SlackBot
-
-
-pid = os.fork()
-if not pid:
-    bot = SlackBot()
-    bot.run()
 
 application = create_app()
